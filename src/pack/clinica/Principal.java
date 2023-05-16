530248ca8c3e11dc6ff38ad18ea7689b48920ab6
lucas <lucas_lfb@hotmail.com>
2023-05-15 21:56:14 -0300

    new

--- src/pack/clinica/Principal.java
+++ src/pack/clinica/Principal.java
@@ -3,10 +3,20 @@ package pack.clinica;
 public class Principal {
     public static void main(String[] args) {
         Paciente p1 = new Paciente();
-        //atributos do paciente
-        p1.peso = 110;
+        Paciente p2 = new Paciente();
+        Paciente p3 = new Paciente();
+        //atributos dos pacientes
+        p1.peso = 102;
         p1.altura = 1.7;
 
+        p2.peso = 68;
+        p2.altura = 1.62;
+
+        p3.peso = 72;
+        p3.altura = 1.81;
+
         p1.diagnostico();
+        p2.diagnostico();
+        p3.diagnostico();
     }
 }
